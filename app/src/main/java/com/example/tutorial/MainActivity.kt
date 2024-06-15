120f1ba7ce3746b0faccbf9dfcfc6a07db84b1b1
teotia-codes <priyanshu.teotia50@gmail.com>
2024-06-15 17:41:52 +0530

    theme switcher functionality added

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -5,14 +5,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -31,27 +37,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        CoroutineScope(Dispatchers.IO).launch {
+/*CoroutineScope(Dispatchers.IO).launch {
             delay(10000)
             DataManager.load(applicationContext)
-        }
+        }*/
         setContent {
-            TutorialTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(),
+            App(modifier = Modifier)
+
+               /* Scaffold(modifier = Modifier.fillMaxSize(),
                     topBar = { TopAppBar(title = { Text(text = "Quotes App", textAlign = TextAlign.Center)
                                                  },
                         ) }) { innerPadding ->
                   App(modifier = Modifier.padding(innerPadding))
+*/
 
-                }
             }
         }
     }
-}
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun App(modifier: Modifier ) {
-    if (DataManager.isDataLoaded){
+   /* if (DataManager.isDataLoaded){
         if (DataManager.currentPage.value == Pages.LISTING){
             HomeScreen(data = DataManager.data, modifier = modifier) {
                DataManager.switchPages(it)
@@ -66,6 +73,19 @@ fun App(modifier: Modifier ) {
 
             CircularProgressIndicator(color = Color.Black)
 
+    }*/
+    var darkTheme by remember {
+            mutableStateOf(false)
+    }
+    TutorialTheme(darkTheme) {
+
+        Scaffold(modifier = modifier) {
+            Column {
+                Button(onClick = { darkTheme = darkTheme.not()}) {
+                    Text(text = "Change theme")
+                }
+            }
+        }
     }
 
 }
