6c0b1bb611c785fc3e072faa0500f93e3b49c4f2
teotia-codes <priyanshu.teotia50@gmail.com>
2024-06-15 16:41:01 +0530

    leading quote icon added

--- app/src/main/java/com/example/tutorial/ui/theme/screens/HomeScreen.kt
+++ app/src/main/java/com/example/tutorial/ui/theme/screens/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.example.tutorial.ui.theme.screens
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,11 +13,15 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.tutorial.R
 import com.example.tutorial.ui.theme.QuoteX
 
 
@@ -41,9 +46,15 @@ fun Quotes(qoute: QuoteX, onClick: () -> Unit) {
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
                 .padding(16.dp)
         ) {
+            Row {
+                Icon(painter = painterResource(id =  R.drawable.baseline_format_quote_24),
+                    contentDescription = "null",
+                    modifier = Modifier.padding(3.dp).rotate(180F))
+            }
             Text(text = qoute.quote)
             HorizontalDivider()
             Text(
