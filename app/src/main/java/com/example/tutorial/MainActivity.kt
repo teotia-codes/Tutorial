7f6a69b20f793de73d6b640976d641c12c45430b
teotia-codes <priyanshu.teotia50@gmail.com>
2024-06-16 21:51:30 +0530

    produce state is used to implement a loader animation

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -8,9 +8,15 @@ import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,13 +28,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tutorial.ui.theme.DataManager
@@ -97,9 +107,10 @@ fun App(modifier: Modifier ) {
             }
         }
     }*/
-  Keyboard()
+ /* Keyboard()
     TextField(value = "", onValueChange = {})
-
+*/
+    Loader(modifier = modifier)
 }
 
 @Composable
@@ -130,6 +141,26 @@ fun Media(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun Loader(modifier: Modifier ) {
+    val degree = produceState(initialValue = 0) {
+        while (true){
+            delay(16)
+            value = (value + 10) % 360
+        }
+    }
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier.fillMaxSize()) {
+            Image(imageVector = Icons.Default.Refresh,
+                contentDescription = "",
+                modifier = Modifier
+                    .size(60.dp)
+                    .rotate(degree.value.toFloat()))
+            Text(text = "Loading")
+        }
+    }
 enum class Pages{
     LISTING,
     DETAIL
